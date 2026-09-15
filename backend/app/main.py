fbb0806dee8be74248db81daf712a7c96f0f526a
MutieWachira <151459388+MutieWachira@users.noreply.github.com>
2026-09-15 20:01:30 +0300

    fix:Fixed CORS bug.

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
         "http://localhost:3000",
         "https://farm-management-system-swart-six.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
